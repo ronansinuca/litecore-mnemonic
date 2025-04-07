050588d418227912c3d1b4e8fd0355f17d352448
ronansinuca <ronan.chaves@hotmail.com>
2025-04-07 20:25:13 -0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = require('./lib/mnemonic');
+//module.exports = require('./lib/mnemonic');
+
+var Mnemonic = require('./lib/mnemonic');//require('bitcore-mnemonic');
+var code = new Mnemonic(Mnemonic.Words.PORTUGUESE);
+var phrase = code.phrase;
+//console.log(phrase)
+console.log(code.toString()); // natal hada sutil año sólido papel jamón combate aula flota ver esfera...
+var wallet = code.toHDPrivateKey();
+
+
+
+console.log(wallet)
+console.log("xpriv", wallet.xprivkey)
+//console.log("publicKey", wallet.privateKey.publicKey)
+console.log("hdPublicKey", wallet.hdPublicKey.xpubkey)
+//console.log("xpubkey", wallet.xpubkey)
